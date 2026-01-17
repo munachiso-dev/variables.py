33b14d7f98e90f2e7ec0f8e302e3b4f342187184
Munachiso Eric Akpuoye <munachisoakpuoye@gmail.com>
2026-01-17 14:16:15 +0000

    Add files via upload

---
+++ variables-program.py
@@ -0,0 +1,26 @@
+x = "Godzilla"
+y = "Kong"
+
+name = 'Skarking'
+print(name)
+name = 'Shimu'
+print(name)
+name = 'Iondragon'
+print(name)
+name = 'Mechagodzilla'
+print(name)
+name = 'Ghidorah'
+print(name)
+name = 'Skullcrawler'
+print(name)
+name = 'Muto'
+print(name)
+
+temperature = 69
+text = "Done"
+
+
+first_name = "Hamato"
+last_name = "Yoshi"
+
+format = "Format"
